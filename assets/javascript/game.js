e190db01420b82ba93df75098d18d30818e0cd3d
Kylie Soderblom <MacDream@Kylies-MacBook-Pro.local>
2016-07-28 15:19:28 -0700

    Thursday afternoon 3pm attempt

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,67 +2,67 @@ var totalScore = 0;
 var wins = 0;
 var losses = 0;
 var counter = 0;
+// latest version as of 7/28 3:20pm
 
 var crystal1 = Math.floor((Math.random()*12)+1);
 var crystal2 = Math.floor((Math.random()*12)+1);
 var crystal3 = Math.floor((Math.random()*12)+1);
 var crystal4 = Math.floor((Math.random()*12)+1);
 var randomNumber = Math.floor(Math.random()*((120-19)+1)+19);
+/*var audio = new Audio ("Magic Wand Noise-SoundBible.com-375928671.wav");*/
 
-// is this really the latest issue?
+		$(document).ready(function(){
+    			console.log( "ready!" );
+    			
+    
+    	$("#random").html(randomNumber);
+   				console.log(randomNumber);
 
-$(document).ready(function(){
-    console.log( "ready!" );
-    totalScore=0; // will this reset value at reload? alert("new score start is 0");
-    console.log (totalScore);
-    $("#random").html(randomNumber);
+   		function setValues(){
+   			$("#Crys1").data ("value",crystal1);
+   			$("#Crys2").data ("value",crystal2);
+   			$("#Crys3").data ("value",crystal3);
+   			$("#Crys4").data ("value",crystal4);
+   			console.log("value")
+   		}
 
-	console.log(randomNumber);
-
-		function crystal() {
-		$(".cImage").click(function(){
-		var score = $(this).data("score");
-		totalScore=(score+totalScore);
-		$("#totalScore").fadeOut('slow')
-		$("#totalScore").html(totalScore);
-		$("#totalScore").fadeIn('slow'); 
-		counter++;
-		console.log(totalScore);
-		console.log(counter);
-		})};
-		
-// move click function outside of if statements.
-// ???assign value to each crys image. change function to class crystals
-// ??this refers to crystals take value in and send value back
-// to element. 
-
-	if (totalScore<randomNumber && counter<11){
-		crystal(); // too late, you need result b4 condition test
-		console.log("first if");
-	}
-	else if (totalScore>randomNumber && counter<11){
-		losses++;
-		counter++;
-		$("#loss").html(losses);
-		alert("Ahhh you lost! Try again");
-		console.log(losses);
-		console.log(counter);
-	}
-	else if (totalScore===randomNumber && counter<11){
-		wins++;
-		counter++;
-		$("#win").html(wins);
-		alert("Congrats - you win!");
-	}
-	else{ // counter=11 - only 10 games then reset 
-		counter++;
-		
-		console.log(console);
-		alert("You've run out of games!")
-		window.location.reload();
-	};
-
+   		$(".cImage").click(function() {
+    		setValues();
+    		$(this).data("value"); 
+			totalScore = totalScore + parseInt($(this).data('value'));
+			$("#totalScore").fadeOut('slow');
+			$("#totalScore").fadeIn('slow');
+			$("#totalScore").html("Your Total Score i*s " + "   " + totalScore);
+    		console.log($(this).data("value"));
 });
+
+
+		if (totalScore<randomNumber && counter<11){
+			console.log("first if - keep going");
+		}
+		else if(totalScore>randomNumber && counter<11){
+			losses++;
+			counter++;
+			$("#loss").html(losses);
+			alert("Ahhh you lost! Try again");
+			console.log(losses);
+			console.log(counter);
+		}
+		else if (totalScore===randomNumber && counter<11){
+			wins++;
+			counter++;
+			$("#win").html(wins);
+			alert("Congrats - you win!");
+		}
+		else{ // counter=11 - only 10 games then reset 
+			counter++;
+			
+			console.log("last else");
+			alert("You've run out of games!")
+			window.location.reload();
+		};
+
+	});
 
  
 	// To do list: add reset button. Add animation / audio 
@@ -70,39 +70,8 @@ $(document).ready(function(){
 	// pull down Game Play instructions. and let slide back up --
 	// using a button - .click slide up/slide down -- stays hidden as default?
 	// if slide down covers / z-index/overflow??? the game below. 
-	//$("#Crys1, Crys2, Crys3, Crys4").click(function() {
-	//if (totalScore<randomNumber){
-		//console.log(totalScore);
-	/*if(totalScore<randomNumber){  // only run if totalScore<randomNumber --- try if 
-
-	$("#Crys1").click(function(){
-	totalScore=(crystal1+totalScore);
-	$("#totalScore").fadeOut('slow');
-	$("#totalScore").html(totalScore);
-	$("#totalScore").fadeIn('slow'); 
-	console.log(totalScore);
-	counter ++
-	});
 	
-
-	/*$("#Crys2").click(function(){
-	totalScore=(crystal2+totalScore);
-	$("#totalScore").replaceWith(totalScore);
-	console.log(totalScore);
-	});
-	$("#Crys3").click(function(){
-	totalScore=(crystal3+totalScore);
-	$("#totalScore").replaceWith(totalScore);
-	console.log(totalScore);
-	});
-	$("#Crys4").click(function(){
-	totalScore=(crystal4+totalScore);
-	$("#totalScore").replaceWith(totalScore);
-	console.log(totalScore);
-	});*/
-
-
-
+	
 
 
 
